cfe56454626b0fb91fde3c5e2beb888e52b2c6b8
Joel Badinas <joelb@thelistinc.com>
2013-12-18 09:54:59 -0500

    Update database.php

--- classes/database.php
+++ classes/database.php
@@ -6,7 +6,7 @@ class Database {
   public $db;
 
   public function __construct() {
-    $this->db = new PDO("mysql:host=localhost;dbname=timemachine;charset=utf8", "root", "crop", array(PDO::ATTR_EMULATE_PREPARES => false,
+    $this->db = new PDO("mysql:host=localhost;dbname=timemachine;charset=utf8", "root", "Wfds323sd", array(PDO::ATTR_EMULATE_PREPARES => false,
                                                                                                             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     $this->class_name = get_class($this);
     $this->table_name = strtolower($this->class_name).'s';
